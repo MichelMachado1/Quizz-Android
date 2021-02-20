6188b774a47c81a94edfa8b65ac5515355009443
MichelMachado1 <53486027+MichelMachado1@users.noreply.github.com>
2021-02-20 13:55:40 +0100

    First Comit

---
+++ app/src/main/java/controller/GameActivity.java
@@ -0,0 +1,134 @@
+package controller;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.topquizz.R;
+
+import java.text.BreakIterator;
+import java.util.Arrays;
+
+import model.Question;
+import model.QuestionBank;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mQuestionTextView;
+    private Button mAnswerButton1;
+    private Button mAnswerButton2;
+    private Button mAnswerButton3;
+    private Button mAnswerButton4;
+
+    private QuestionBank mQuestionBank;
+    private Question mCurrentQuestion;
+
+    private int mNumberOfQuestions;
+    private int mScore;
+
+    public static final String BUNDLE_EXTRA_SCORE = "BUNDLE_EXTRA_SCORE";
+    public static final String BUNDLE_STATE_SCORE = "currentScore";
+    public static final String BUNDLE_STATE_QUESTION = "currentQuestion";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mQuestionBank = this.generateQuestion();
+        mNumberOfQuestions = 4;
+        mQuestionTextView = (TextView) findViewById(R.id.activity_game_question_text);
+        mAnswerButton1 = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswerButton2 = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswerButton3 = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswerButton4 = (Button) findViewById(R.id.activity_game_answer4_btn);
+
+        mAnswerButton1.setTag(0);
+        mAnswerButton2.setTag(1);
+        mAnswerButton3.setTag(2);
+        mAnswerButton4.setTag(3);
+
+        mAnswerButton1.setOnClickListener(this);
+        mAnswerButton2.setOnClickListener(this);
+        mAnswerButton3.setOnClickListener(this);
+        mAnswerButton4.setOnClickListener(this);
+
+        mCurrentQuestion = mQuestionBank.getQuestion();
+        this.displayQuestion(mCurrentQuestion);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int responseIndex = (int)v.getTag();
+        if (responseIndex == mCurrentQuestion.getAnswerIndex()){
+            mScore++;
+            Toast.makeText(this,"correct",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"wrong answer",Toast.LENGTH_SHORT).show();
+        }
+        if (--mNumberOfQuestions == 0) {
+            // No question left, end the game
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("Well done!")
+                    .setMessage("Your score is " + mScore)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .create()
+                    .show();
+        } else {
+            mCurrentQuestion = mQuestionBank.getQuestion();
+            displayQuestion(mCurrentQuestion);
+        }
+    }
+
+    private void displayQuestion(final Question question) {
+        mQuestionTextView.setText(question.getQuestion());
+        mAnswerButton1.setText(question.getChoiceList().get(0));
+        mAnswerButton2.setText(question.getChoiceList().get(1));
+        mAnswerButton3.setText(question.getChoiceList().get(2));
+        mAnswerButton4.setText(question.getChoiceList().get(3));
+    }
+
+    
+
+    private QuestionBank generateQuestion() {
+        Question question1 = new Question("Who is the creator of Android?",
+                Arrays.asList("Andy Rubin",
+                        "Steve Wozniak",
+                        "Jake Wharton",
+                        "Paul Smith"),
+                0);
+
+        Question question2 = new Question("When did the first man land on the moon?",
+                Arrays.asList("1958",
+                        "1962",
+                        "1967",
+                        "1969"),
+                3);
+
+        Question question3 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42",
+                        "101",
+                        "666",
+                        "742"),
+                3);
+        Question question4 = new Question("What is the longest river in the world?",
+                Arrays.asList("Mississippi","Amazon","River Nile","Rio Grande"),2);
+        return new QuestionBank(Arrays.asList(question1,
+                question2,
+                question3,
+                question4));
+    }
+}
---
+++ app/src/main/java/controller/MainActivity.java
@@ -0,0 +1,126 @@
+package controller;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.topquizz.R;
+
+import model.User;
+
+public class MainActivity extends AppCompatActivity
+{
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+    private User mUser;
+    public static final int GAME_ACTIVITY_REQUEST_CODE = 42;
+    private SharedPreferences mPreferences;
+    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+    public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mUser = new User();
+
+        mPreferences = getPreferences(MODE_PRIVATE);
+
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+        mPlayButton.setEnabled(false);
+
+        greetUser();
+
+        mNameInput.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                mPlayButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                String firstname = mNameInput.getText().toString();
+                mUser.setFirstName(firstname);
+
+                mPreferences.edit().putString(PREF_KEY_FIRSTNAME, mUser.getFirstName()).apply();
+
+                Intent gameActivityItent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivityItent);
+
+                startActivityForResult(gameActivityItent, GAME_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+
+
+
+
+
+
+
+        }
+    @SuppressLint("MissingSuperCall")
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        if (GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode)
+        {
+            // Fetch the score from the Intent
+            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
+            mPreferences.edit().putInt(PREF_KEY_SCORE, score).apply();
+        }
+    }
+
+    private void greetUser() {
+        String firstname = mPreferences.getString(PREF_KEY_FIRSTNAME, null);
+
+
+        if (null != firstname) {
+            int score = mPreferences.getInt(PREF_KEY_SCORE, 0);
+
+            String fulltext = "Welcome back, " + firstname
+                    + "!\nYour last score was " + score
+                    + ", will you do better this time?";
+            mGreetingText.setText(fulltext);
+            mNameInput.setText(firstname);
+            mNameInput.setSelection(firstname.length());
+            mPlayButton.setEnabled(true);
+        }
+    }
+}
+
+
